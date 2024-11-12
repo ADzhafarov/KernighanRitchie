120bf714303a5e542d739536a60ff0056e454bd4
genzonline <genzonlinew@gmail.com>
2024-11-12 22:43:58 +0400

    Ex4-5

--- c4/polish-calc.c
+++ c4/polish-calc.c
@@ -1,9 +1,27 @@
+#include <math.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAXOP		100
-#define NUMBER		'0'
+
+enum operations {
+	ADD = '+',
+	SUB = '-',
+	MUL = '*',
+	DIV = '/',
+	MOD = '%',
+	NUMBER = 1,
+	PRINT,
+	DUPLICATE,
+	SWAP,
+	SIN,
+	COS,
+	EXP,
+	POW,
+	UNDEFINED,
+};
 
 int getop(char []);
 void push(double);
@@ -19,45 +37,58 @@ int main(void) {
 		case NUMBER:
 			push(atof(s));
 			break;
-		case '+':
+		case ADD:
 			push(pop() + pop());
 			break;
-		case '*':
+		case MUL:
 			push(pop() * pop());
 			break;
-		case '-':
+		case SUB:
 			op2 = pop();
 			push(pop() - op2);
 			break;
-		case '/':
+		case DIV:
 			op2 = pop();
 			if (op2 != 0.0)
 				push(pop() / op2);
 			else
 				printf("error: zero division\n");
 			break;
-		case '%':
+		case MOD:
 			op2 = pop();
 			if (op2 != 0.0)
 				push((int)pop() % (int)op2);
 			else
 				printf("error: zero division\n");
 			break;
-		case 'p': /* print */
+		case PRINT:
 			op = pop();
 			printf("\t%.8g\n", op);
 			push(op);
 			break;
-		case 'd': /* duplicate */
+		case DUPLICATE:
 	 		op = pop();
 			push(op);
 			push(op);
 			break;
-		case 's': /* swap */
+		case SWAP:
 			op = pop();
 			op2 = pop();
 			push(op);
 			push(op2);
+			break;
+		case SIN:
+			push(sin(pop()));
+			break;
+		case COS:
+			push(cos(pop()));
+			break;
+		case EXP:
+			push(exp(pop()));
+			break;
+		case POW:
+			op2 = pop();
+			push(pow(pop(), op2));
 			break;
 		case '\n':
 			printf("\t%.8g\n", pop());
@@ -95,36 +126,77 @@ double pop(void) {
 int getch(void);
 void ungetch(int);
 
+void getword(char s[], int maxlen);
+
 int getop(char s[]) {
+	int i;
+
+	getword(s, MAXOP);
+
+	if (s[1] == '\0' && !isdigit(s[0]))
+		return s[0];
+
+	if (isdigit(s[0]) || s[0] == '.' || s[0] == '-') {
+		i = 0;
+		while (isdigit(s[++i]))
+			;
+
+		if (s[i] == '.')
+			while (isdigit(s[++i]))
+				;
+		s[i] = '\0';
+
+		return NUMBER;
+	}
+	
+	
+
+	if (strcmp(s, "print") == 0)
+		return PRINT;
+	
+	if (strcmp(s, "duplicate") == 0)
+		return DUPLICATE;
+
+	if (strcmp(s, "swap") == 0)
+		return SWAP;
+
+	if (strcmp(s, "sin") == 0)
+		return SIN;
+
+	if (strcmp(s, "cos") == 0)
+		return COS;
+
+	if (strcmp(s, "exp") == 0)
+		return EXP;
+
+	if (strcmp(s, "pow") == 0)
+		return POW;
+
+	return UNDEFINED;
+}
+
+void getword(char s[], int maxlen) {
 	int i, c;
 
-	while ((s[0] = c = getch()) == ' ' || c == '\t')
+	i = 0;
+	while ((c = getch()) == ' ' || c == '\t')
 		;
-	s[1] = '\0';
-	if (!isdigit(c) && c != '.' && c != '-')
-		return c;
+	s[i++] = c;
 
-	if (c == '-') {
-		if ((c = getch()) == ' ' || c == '\t' || c == '\n' || c == EOF) {
-			if (c == '\n')
-				ungetch(c);
-			return '-';
-		}
-		else ungetch(c);
+	if (s[0] == '\n') {
+		s[1] = '\0';
+		return;
 	}
 
-	i = 0;
-	if (isdigit(c))
-		while (isdigit(s[++i] = c = getch()))
-			;
-	if (c == '.')
-		while (isdigit(s[++i] = c = getch()))
-			;
-	s[i] = '\0';
-	if (c != EOF)
+	while ((c = getch()) != ' ' && c != '\t' && c != '\n' && c != EOF && i < maxlen - 1)
+		s[i++] = c;
+
+	if (c == '\n' || c == EOF)
 		ungetch(c);
-	return NUMBER;
+
+	s[i] = '\0';
 }
+
 
 #define BUFSIZE		100
 
