693a8d9409b6988e4bdfa67f514d3ea8a2a098c7
genzonline <genzonlinew@gmail.com>
2024-11-21 21:28:45 +0400

    Ex5-11 (detab)

---
+++ c5/Ex5-11p2.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define IN			1
+#define OUT			0
+#define MAXSTOPS	20
+
+int next_tabstop(int pos, int *tabstops);
+
+int main(int argc, char *argv[]) {
+	int c, i, l, state, t;
+	int tabstops[MAXSTOPS];
+
+	for (i = 0; i < MAXSTOPS; i++)
+		tabstops[i] = --argc > 0 ? atoi(*++argv) : tabstops[i-1] + 4;
+
+	int next_t = 0;
+	l = t = 0;
+	state = OUT;
+	while ((c = getchar()) != EOF) {
+		if (c == ' ') {
+			++l;
+			state = OUT;
+		} else {
+			if (state == OUT) {
+				if (l == 1 || next_tabstop(t, tabstops) == next_tabstop(t + l, tabstops)) {
+					for (i = 0; i < l; ++i)
+						printf("%c", ' ');
+					t += l;
+				} else {
+					while ((next_t = next_tabstop(t, tabstops)) < next_tabstop(t + l, tabstops)) {
+						l -= next_t - t;
+						t = next_t;
+						printf("%c", '\t');
+					}
+					for (i = 0; i < l; ++i) 
+						printf("%c", ' ');
+				}
+				state = IN;
+				l = 0;
+			}
+
+			if (c == '\n')
+				t = 0;
+
+			printf("%c", c);
+			++t;
+		}
+	}
+}
+
+int next_tabstop(int pos, int *tabstops) {
+	while (*tabstops++ <= pos)
+		;
+
+	return *--tabstops;
+}
